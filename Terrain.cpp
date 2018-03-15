7ba662388f9de53fc6e65576876f2c61e667c93f
Ronald Baldonado <ronbaldona@yahoo.com>
2018-03-14 20:06:55 -0700

    Added basis for Terrain loading

---
+++ Terrain.cpp
@@ -0,0 +1,90 @@
+#include "Terrain.h"
+
+Terrain::Terrain() {
+	xz_scale = 1.0f;
+	height_scale = 1.0f;
+
+	init_buffers();
+}
+
+Terrain::Terrain(float xz_scale, float height_scale) {
+	this->xz_scale = xz_scale;
+	this->height_scale = height_scale;
+
+	init_buffers();
+}
+
+Terrain::~Terrain() {
+	// Delete previously generated buffers. Note that forgetting to do this can waste GPU memory in a 
+	// large project! This could crash the graphics driver due to memory leaks, or slow down application performance!
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+}
+
+void Terrain::init_buffers() {
+
+}
+
+/** Load a ppm file from disk.
+@input filename The location of the PPM file.  If the file is not found, an error message
+will be printed and this function will return 0
+@input width This will be modified to contain the width of the loaded image, or 0 if file not found
+@input height This will be modified to contain the height of the loaded image, or 0 if file not found
+@return Returns the RGB pixel data as interleaved unsigned chars (R0 G0 B0 R1 G1 B1 R2 G2 B2 .... etc) or 0 if an error ocured
+**/
+unsigned char* Terrain::loadPPM(const char* filename, int& width, int& height)
+{
+	const int BUFSIZE = 128;
+	FILE* fp;
+	unsigned int read;
+	unsigned char* rawData;
+	char buf[3][BUFSIZE];
+	char* retval_fgets;
+	size_t retval_sscanf;
+
+	if ((fp = fopen(filename, "rb")) == NULL)
+	{
+		std::cerr << "error reading ppm file, could not locate " << filename << std::endl;
+		width = 0;
+		height = 0;
+		return NULL;
+	}
+
+	// Read magic number:
+	retval_fgets = fgets(buf[0], BUFSIZE, fp);
+
+	// Read width and height:
+	do
+	{
+		retval_fgets = fgets(buf[0], BUFSIZE, fp);
+	} while (buf[0][0] == '#');
+	retval_sscanf = sscanf(buf[0], "%s %s", buf[1], buf[2]);
+	width = atoi(buf[1]);
+	height = atoi(buf[2]);
+
+	// Read maxval:
+	do
+	{
+		retval_fgets = fgets(buf[0], BUFSIZE, fp);
+	} while (buf[0][0] == '#');
+
+	// Read image data:
+	rawData = new unsigned char[width * height * 3];
+	read = fread(rawData, width * height * 3, 1, fp);
+	fclose(fp);
+	if (read != 1)
+	{
+		std::cerr << "error parsing ppm file, incomplete data" << std::endl;
+		delete[] rawData;
+		width = 0;
+		height = 0;
+		return NULL;
+	}
+
+	return rawData;
+}
+
+void Terrain::draw(GLuint shaderProgram) {
+
+}
---
+++ Terrain.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef _TERRAIN_H_
+#define _TERRAIN_H_
+
+#define GLFW_INCLUDE_GLEXT
+#ifdef __APPLE__
+#define GLFW_INCLUDE_GLCOREARB
+#else
+#include <GL/glew.h>
+#endif
+#include <GLFW/glfw3.h>
+// Use of degrees is deprecated. Use radians instead.
+#ifndef GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS
+#endif
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+class Terrain {
+	glm::mat4 toWorld;
+
+	float xz_scale;
+	float height_scale;
+
+	GLuint VBO, VAO, EBO;
+	GLuint uProjection, uModelview, uView, uMode;
+	GLuint textureID;
+
+public:
+	Terrain();
+	Terrain(float, float);
+	~Terrain();
+
+	void init_buffers();
+	unsigned char* loadPPM(const char* filename, int& width, int& height);
+	void draw(GLuint);
+};
+
+// Hardcoded floor for checking if texture code works
+const GLfloat vertices[] = {
+	-500.0f,  500.0f, -500.0f,
+	500.0f,  500.0f, -500.0f,
+	500.0f,  500.0f,  500.0f,
+	500.0f,  500.0f,  500.0f,
+	-500.0f,  500.0f,  500.0f,
+	-500.0f,  500.0f, -500.0f,
+};
+
+#endif
